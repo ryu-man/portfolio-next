506ca4a493cedb9f8640424389d4b854116d39e8
abdelhalim <rioman19@gmail.com>
2025-04-25 09:20:30 -0400

    improve text size

--- src/lib/components/stats.tsx
+++ src/lib/components/stats.tsx
@@ -1,28 +1,28 @@
 const Stats = () => {
   return (
     <div className="sd:text-8xl flex w-full flex-wrap justify-between py-8 text-3xl">
-      <div className="sd:flex-row flex flex-col items-center gap-2">
+      <div className="sd:flex-row flex flex-col items-center gap-2 text-5xl md:text-7xl lg:text-8xl">
         <div className="font-bold">4</div>
         <div className="sd:text-end w-min text-center text-sm text-wrap">
           Years of expiernce
         </div>
       </div>
 
-      <div className="sd:flex-row flex flex-col items-center gap-2">
+      <div className="sd:flex-row flex flex-col items-center gap-2 text-5xl md:text-7xl lg:text-8xl">
         <div className="font-bold">26</div>
         <div className="sd:text-end w-min text-center text-sm text-wrap">
           Projects Compoleted
         </div>
       </div>
 
-      <div className="sd:flex-row flex flex-col items-center gap-2">
+      <div className="sd:flex-row flex flex-col items-center gap-2 text-5xl md:text-7xl lg:text-8xl">
         <div className="font-bold">4</div>
         <div className="sd:text-end w-min text-center text-sm text-wrap">
           Modern Frameworks
         </div>
       </div>
 
-      <div className="sd:flex-row flex flex-col items-center gap-2">
+      <div className="sd:flex-row flex flex-col items-center gap-2 text-5xl md:text-7xl lg:text-8xl">
         <div className="font-bold">500</div>
         <div className="sd:text-end w-min text-center text-sm text-wrap">
           Code commits
